ee2e1ba88b55e387b129e4dc43f9bd187296c908
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-11-17 01:38:12 -0600

    going to bed

--- modules/nearEarthObjectsHandler.js
+++ modules/nearEarthObjectsHandler.js
@@ -1,12 +1,17 @@
 import settings from "./settings.js"
 
 export default {
-    getToday(start) {
 
+    getByDate(start) {
         settings.keySet()
             .then((res) => fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&api_key=${res}`))
             .then(res => res.json())
             .then(res => console.log(res))
     },
+    browseNEOByPage(pageNumber) {
+        settings.keySet()
+            .then((res) => fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?page=${pageNumber}&api_key=${res}`))
+
+    }
 
 }
