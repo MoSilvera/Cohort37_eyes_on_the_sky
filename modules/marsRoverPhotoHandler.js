870f4586e11709955fc94294571f358838e0dc8a
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-11-17 22:01:47 -0600

    functioning martian rover html display

--- modules/marsRoverPhotoHandler.js
+++ modules/marsRoverPhotoHandler.js
@@ -4,9 +4,11 @@ export default {
 
     //gets mars rover pictures by date
     getByDate(start) {
-        settings.keySet()
+       let list = settings.keySet()
             .then((res) => fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${start}&api_key=${res}`))
-            .then(res => res.json())
-            .then(res => console.log(res))
+            .then((res) => res.json())
+
+            return list
+
     }
 }
--- modules/pictureOfTheDayHandler.js
+++ modules/pictureOfTheDayHandler.js
@@ -3,10 +3,9 @@ import settings from "./settings.js"
 export default {
     //gets the astronomy picture of te day
     getToday() {
-      settings.keySet()
+      return settings.keySet()
       .then(res => fetch(`https://api.nasa.gov/planetary/apod?api_key=${res}`))
       .then(res => res.json())
-      .then(res => console.log(res))
     }
 
 }
