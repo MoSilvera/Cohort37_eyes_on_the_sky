890a7430fbd72181ec812c859bc366a2d203b3de
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-11-17 22:47:58 -0600

    dynamic rover addition to fetch

--- modules/marsRoverPhotoHandler.js
+++ modules/marsRoverPhotoHandler.js
@@ -3,9 +3,9 @@ import settings from "./settings.js"
 export default {
 
     //gets mars rover pictures by date
-    getByDate(start) {
+    getByDate(start, rover) {
        let list = settings.keySet()
-            .then((res) => fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${start}&api_key=${res}`))
+            .then((res) => fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${start}&api_key=${res}`))
             .then((res) => res.json())
 
             return list
