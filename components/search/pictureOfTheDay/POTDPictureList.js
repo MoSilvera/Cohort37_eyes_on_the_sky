1f6b05aa9c45b7d2f24165ed85209f06d025ebe3
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-11-18 10:11:33 -0600

    dialog box added

--- components/search/pictureOfTheDay/POTDPictureList.js
+++ components/search/pictureOfTheDay/POTDPictureList.js
@@ -1,7 +1,26 @@
 import PictureOfTheDayHandler from "../../../modules/PictureOfTheDayHandler.js"
 import PictureOfTheDayComponent from "./PictureOfTheDay.js"
 
+const initializeDetailButtonEvents = () => {
+
+   document.querySelector(".root").addEventListener("click", (evt) => {
+        const detailsDialog = document.getElementById("dialog-POTD")
+
+       if (evt.target.id === "details--POTD")
+       {
+            detailsDialog.showModal()
+       }
+       if (evt.target.id === "close-POTD")
+       {
+            detailsDialog.close()
+       }
+})
+
+}
+
 const POTDPictureList = (date) => {
+
+    initializeDetailButtonEvents()
 
     const rootElement = document.querySelector(".root")
 
@@ -11,6 +30,8 @@ const POTDPictureList = (date) => {
             rootElement.innerHTML += PictureOfTheDayComponent(res)
         }
         )
+        .then(() => initializeDetailButtonEvents())
+
 
 }
 
--- components/search/pictureOfTheDay/PictureOfTheDay.js
+++ components/search/pictureOfTheDay/PictureOfTheDay.js
@@ -1,24 +1,25 @@
 
 const PictureOfTheDayComponent = (object) => {
+
     return `
         <div class="potd">
             <h4 class=rover_name>${object.title}</h4>
+
             <div class="potd_image">
                 <img class="" src="${object.url}" alt="">
             </div>
+
             <div class="rover_details">
-                <ul>
-                    <li>
                         Date: ${object.date}
-                    </li>
-                    <li>
-                        Description: ${object.explanation}
-                    </li>
-                    <li>
-                    &copy; ${object.copyright}
-                    </li>
-                </ul>
             </div>
+
+            <button id="details--POTD">Details</button>
+
+            <dialog id="dialog-POTD" >
+            <div> Description: ${object.explanation} </div>
+            <button class="button--close" id="close-POTD">Close</button>
+            </dialog>
+
         </div>
     `
 }
