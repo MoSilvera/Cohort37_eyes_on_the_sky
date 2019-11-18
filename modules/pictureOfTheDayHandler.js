4108b9a76f5d8d083560a0ebad5014c1d804d5ba
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-11-17 23:10:42 -0600

    adding picture of the day components

--- modules/pictureOfTheDayHandler.js
+++ modules/pictureOfTheDayHandler.js
@@ -2,9 +2,9 @@ import settings from "./settings.js"
 
 export default {
     //gets the astronomy picture of te day
-    getToday() {
+    getByDate(date) {
       return settings.keySet()
-      .then(res => fetch(`https://api.nasa.gov/planetary/apod?api_key=${res}`))
+      .then(res => fetch(`https://api.nasa.gov/planetary/apod?api_key=${res}&date=${date}`))
       .then(res => res.json())
     }
 
